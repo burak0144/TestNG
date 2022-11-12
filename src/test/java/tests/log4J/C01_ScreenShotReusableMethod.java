f33782b009597d9a219a02a94358702ec196c03e
burak <karanfilkaranfil822@gmail.com>
2022-11-12 15:50:17 +0300

    "01.10.22"

---
+++ src/test/java/tests/log4J/C01_ScreenShotReusableMethod.java
@@ -0,0 +1,36 @@
+package tests.log4J;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+import java.io.IOException;
+public class C01_ScreenShotReusableMethod {
+    private static Logger logger = LogManager.getLogger(C01_ScreenShotReusableMethod.class.getName());
+    @Test
+    public void test01() throws IOException {
+
+        logger.info("Hepsi burada sitesine gidilir");
+        //Hepsiburada sayfasına gidiniz
+        Driver.getDriver().get("https://hepsiburada.com");
+        //Ve sayfanın resmini alınız
+        logger.info("Ekran goruntusu alinir");
+        ReusableMethods.getScreenshot("hepsiburada");
+        logger.warn("Driver kapatilir");
+        //Sayfayı kapatınız
+        Driver.closeDriver();
+    }
+    @Test
+    public void test02() throws IOException {
+        //Amazon sayfasına gidiniz
+        Driver.getDriver().get(ConfigReader.getProperty("amznUrl"));
+        AmazonPage amznPage = new AmazonPage();
+        //Nutella aratınız
+        amznPage.aramaKutusu.sendKeys("Nutella", Keys.ENTER);
+        //Arama sonuc yazısı Webelementinin resmini alınız
+        ReusableMethods.getScreenshotWebElement("AramaSonucWE",amznPage.aramaKutusu);
+    }
+}
